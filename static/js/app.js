70c59f338febda1bb709043425bbebcf412e00a7
Matthew <ashlander@live.com>
2021-01-13 01:36:39 -0600

    Finished level 1

--- static/js/app.js
+++ static/js/app.js
@@ -4,17 +4,39 @@ var tableData = data;
 // YOUR CODE HERE!
 var table = d3.select('#ufo-table');
 
-//Append table row
+//Append table function
 function appendrow(row) {
 	console.log(row);
 	var trow = table.append('tr');
 	trow.append('td');
 	Object.entries(row).forEach(function([key, value]) {
 		var cell = table.append("td");
-		cell.text(value)
+		cell.text(value);
 	});
 };
 
+//Call function for each row
 for (row = 0; row < tableData.length; row++) {
 	appendrow(tableData[row]);
 };
+
+var button = d3.select('#filter-btn');
+var field = d3.select('#datetime');
+var form = d3.select('form');
+
+//search button function
+function search() {
+	d3.event.preventDefault();
+	var userinput = field.property('value');
+	console.log(userinput);
+	var filtered = tableData.filter(sighting => sighting.datetime == userinput);
+		
+	// create rows for filtered data
+	table.html('');
+	for (row = 0; row < filtered.length; row++) {
+		appendrow(filtered[row]);
+	};
+};
+
+button.on('click', search);
+form.on('submit', search);
